1463c1199009739873cde183745efbbbbc84d5ea
Asher Payn <asherpayn@gmail.com>
2026-03-02 13:12:47 +0000

    Add basic user login input functionality
    Initial implementation of a login program that collects user details.

---
+++ Python/login.py
@@ -0,0 +1,16 @@
+# This is supposed to be a program that allows a user to log in.
+
+print("Please enter your details, note these will be used for the login form later")
+user = input("Please enter your name: ")
+# password = input("Please enter your password: ") later :)
+age = int(input("Please enter your age: "))
+dob = input("Please enter your date of birth dd/mm/yy: ")
+
+print(" ") # TODO: find out how to print a new line (cant google during test)
+print("current name:", user, "current age:", age)
+
+print(" ")
+print("Starting login process. . .")
+password = input("Please enter your password: ")
+
+print("unfinished, use if statements to check.")
